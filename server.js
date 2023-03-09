fb536369ea6567047d8d1897ee92ec070d094282
pratiksha <a@gmail.com>
2023-03-09 19:36:06 +0530

    modify package

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+
 mongoose.connect('mongodb://127.0.0.1:27017/task1', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
